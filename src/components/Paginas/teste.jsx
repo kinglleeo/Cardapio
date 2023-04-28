751164e3d5341bcf1ec84210473f2e129bb65fe8
kinglleeo <leonardolauerpessoal@outlook.com>
2023-04-28 17:58:15 -0300

    awds

--- src/components/Paginas/teste.jsx
+++ src/components/Paginas/teste.jsx
@@ -9,27 +9,26 @@ export default function teste(){
     return(
         <div className='teste-main'>
             <div className='caxinha-do-teste'>
-                <div className='comanda'>
-                    <div className='caixa-c'>
-                        <div className='item-c'>
-                            <div className='item-c-1'>
-                                <div>Pizza Grande</div>
-                            </div>
-                            <div className='item-c-2'>
-                                <div>
-                                    <div>Total</div>
-                                    <div>R$ 000,00</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='caixa-d'>
-                        <div className='caixa-d-items'>
-                            <div>Nome</div>
-                            <div>nome1, nome2, nome3, nome4</div>
-                        </div>
-                    </div>
-                </div>
+            <div class="main">
+  <div class="up">
+    <button class="card1">
+      a
+    </button>
+    <button class="card2">
+    b
+    </button>
+  </div>
+  <div class="down">
+    <button class="card3">
+      c
+    </button>
+    <button class="card4">
+    d
+    </button>
+  </div>
+</div>
+            
+            
             </div>
         </div>
     )
