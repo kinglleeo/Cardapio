6af176c4ed18c1db0c278edfa5837b3e0bd8c9a9
leonardo adriano lauer <45231828+kinglleeo@users.noreply.github.com>
2023-05-10 21:34:16 -0300

    Create Adicionais.jsx

---
+++ src/components/Paginas/Produtos/Adicionais.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { formCurrency } from '../../../AA-utilidades/numeros';
+import IconCarrinho from '../../../Carrinho/Iconcarrinho';
+import TopoPagina from '../../../AA-utilidades/Topo';
+import { api } from '../../../../conecções/api';
+import Decimal from 'decimal.js';
+import { useLocation } from 'react-router-dom';
+
+export default function AdicionaisList() {
+  const [adicionais, setAdicionais] = useState([]);
+  const { state } = useLocation();
+  const { item } = state;
+  const [quantidade, setQuantidade] = useState(0);
+
+  useEffect(() => {
+    api.get('/Adicionais').then((getdata) => {
+      setAdicionais(getdata.data);
+    });
+  }, []);
+
+  const handleIncreaseQuantidade = () => {
+    setQuantidade(quantidade + 1);
+  };
+
+  const handleDecreaseQuantidade = () => {
+    if (quantidade > 0) {
+      setQuantidade(quantidade - 1);
+    }
+  };
+
+  const valorTotal = Decimal(item.valor)
+    .mul(quantidade)
+    .toFixed(2); // Assuming the item.valor is a numeric value
+
+  return (
+    <div>
+      <TopoPagina valor={valorTotal} /> {/* Display initial item.valor and updated total value */}
+      {adicionais.map((adicional) => (
+        <div key={adicional.id}>
+          <h3>{adicional.nome}</h3>
+          <p>{adicional.descricao}</p>
+          <p>{formCurrency(adicional.valor)}</p>
+          <button onClick={handleIncreaseQuantidade}>+</button>
+          <span>{quantidade}</span>
+          <button onClick={handleDecreaseQuantidade}>-</button>
+        </div>
+      ))}
+    </div>
+  );
+}
