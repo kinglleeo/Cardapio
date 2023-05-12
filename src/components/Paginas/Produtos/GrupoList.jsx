d3c1602d77ce152f60c4a7f1cfd4d9aa1a02990a
kinglleeo <leonardolauerpessoal@outlook.com>
2023-05-12 17:37:14 -0300

    12/05/2023

--- src/components/Paginas/Produtos/GrupoList.jsx
+++ src/components/Paginas/Produtos/GrupoList.jsx
@@ -10,12 +10,12 @@ export default function Grupo({ grupos, setSubGrupoList }){
         {Array.isArray(grupos) ? grupos.map((item)=>
             <div className='GrupoList' id={item.ID_GRUPO} key={item.ID_GRUPO}>
                 <div className='Grupo-Titulo'>{item.GRUPO}</div>
-                <div>
-                    <SubGrupoList
-                        ID_GRUPO={item.ID_GRUPO}
-                        setSubGrupoList={setSubGrupoList}
-                    />
-                </div>
+                    <div>
+                        <SubGrupoList
+                            ID_GRUPO={item.ID_GRUPO}
+                            setSubGrupoList={setSubGrupoList}
+                        />
+                    </div>
             </div>
         ) : null}        
    </div>
