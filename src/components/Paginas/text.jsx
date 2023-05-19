7d8170f76d32003a282271cd6204b96a05542ec7
kinglleeo <leonardolauerpessoal@outlook.com>
2023-05-19 10:03:00 -0300

    19/05/2023

---
+++ src/components/Paginas/text.jsx
@@ -0,0 +1,16 @@
+const queryClient = useQueryClient();
+const listaOpcionais = queryClient.getQueryData(['listaOpcionais', grupoOpcoes, idProduto]);
+
+useEffect(() => {
+    const calcularTotal = () => {  
+      let valorTotal = new Decimal(0);
+        listasAdicionais.forEach((item) => {
+          const quantidade = item.quantidade;
+          const valor = new Decimal(item.VALOR_VENDA || 0);
+          const subtotal = valor.times(quantidade);
+            valorTotal = valorTotal.plus(subtotal);
+        });
+      return formCurrency.format(valorTotal)
+    };
+  console.log(calcularTotal())
+  }, [listasAdicionais]);
