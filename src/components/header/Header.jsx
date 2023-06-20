f25b682ae8609e817f5001dbaede5789db2e580b
kinglleeo <leonardolauerpessoal@outlook.com>
2023-06-20 11:48:46 -0300

    20/06/2023

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,21 +4,31 @@ import UsuarioBar from '../Usuarios/UsuarioInfo/UsuarioBar'
 
 export default function Header (){
     return(
-        <div className='header-body'>
-            <div> 
-                <UsuarioBar/>
+        <div className='Main-header'>
+            <div className='header-header'>
+                <div className='logo-garline'></div>
             </div>
-            <div className='header-caixa' >
-                <div className='caixa-conteudo'>
-                    <div className='header-cardapio'>
-                        <div>Cardapio</div>
-                    </div>
-                        <div className='header-00'>
-                            <div className='header-tempo'>Tempo de Espera: </div>
-                            <div className='header-formas'>Formas de Pagamento: </div>
+            <div className='body-header'>
+                <div className='banner-garline'>
+                    <div className='banner-infos'>
+                        <div className='icone-tempo'> </div>
+                        <div className='tempo-banner'>
+                            10 - 20 minutos
                         </div>
+                    </div>
+                    <div className='banner-infos'>
+                        <div className='icone-fechar'>
+                            <img url='https://png.pngtree.com/png-vector/20220918/ourmid/pngtree-red-closed-sign-design-for-store-or-shope-png-image_6181316.png'></img>
+                        </div>
+                        <div className='tempo-banner'>
+                            Fecha as 23:00
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <div className='footer-header'>
+                    <div className='endereço-header'> Rua sjisjajsijsiadjiajsidjasd, shdaijsdoaokdsa - sajdpsa </div>
+                </div>
+            </div>    
         </div>
     )
 }
