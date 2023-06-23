e9964c0f1f2c4eb459df16368e288ff301e02dbd
kinglleeo <leonardolauerpessoal@outlook.com>
2023-06-23 13:39:18 -0300

    23/06/2023

--- src/components/Paginas/Produtos/Adicionais/AdicionaisPorQuantidade.jsx
+++ src/components/Paginas/Produtos/Adicionais/AdicionaisPorQuantidade.jsx
@@ -1,11 +1,36 @@
-<div className='Card-Adicionais-Botoes'>
-                          <div className='btn-quantia-adicionais'>
-                            <button className='arrow left' onClick={() => diminuirQuantidade(index)}></button>
-                          </div>
-                          <div className='quantia-adicionais'>{item.quantidade}</div>
-                          <div className='btn-quantia-adicionais'>
-                            <button className='arrow right'onClick={() => aumentarQuantidade(index)}
-                                disabled={Faltam() === 0}
-                            ></button>
-                          </div>
-                        </div>
+import { React, useState, useEffect } from 'react'
+
+
+export default function AdicionaisPorQuantidade(){
+
+
+  const aumentarQuantidade = (index) => {
+    const updatedListaOpcionais = [...listaAdicionais];
+    const quantidade = new Decimal(updatedListaOpcionais[index].quantidade);
+    updatedListaOpcionais[index].quantidade = quantidade.plus(1).toNumber();
+        setListaAdicionais(updatedListaOpcionais);
+  };
+
+  const diminuirQuantidade = (index) => {
+    const updatedListaOpcionais = [...listaAdicionais];
+    const quantidade = new Decimal(updatedListaOpcionais[index].quantidade);
+    if (quantidade.gt(0)) {
+    updatedListaOpcionais[index].quantidade = quantidade.minus(1).toNumber();
+        setListaAdicionais(updatedListaOpcionais);
+    }
+  };
+
+
+
+  return(
+    <div className='Card-Adicionais-Botoes'>
+      <div className='btn-quantia-adicionais'>
+        <button className='arrow left' onClick={() => diminuirQuantidade(index)}></button>
+      </div>
+        <div className='quantia-adicionais'>{item.quantidade}</div>
+      <div className='btn-quantia-adicionais'>
+        <button className='arrow right'onClick={() => aumentarQuantidade(index)} disabled={Faltam() === 0}></button>
+      </div>
+    </div>
+  )
+}
