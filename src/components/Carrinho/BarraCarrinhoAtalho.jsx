dcab19051368c051e2767f4cb1c7c97d525f7d31
kinglleeo <leonardolauerpessoal@outlook.com>
2023-06-29 17:58:10 -0300

    29/06/2023

--- src/components/Carrinho/BarraCarrinhoAtalho.jsx
+++ src/components/Carrinho/BarraCarrinhoAtalho.jsx
@@ -29,15 +29,13 @@ export default function BarraCarrinhoAtalho() {
   return (
     <Link to='/carrinho' style={{ color: 'inherit', textDecoration: 'inherit' }}>
       <div className='carrinho'>
-        <div className='caixa-carrinho'>
-            <div className='totalQuantidade-cart'>{getTotalQuantity() || 0} X</div>
-            <div className='totalQuantidade-cart-svg'>
-              {getTotalQuantity() > 0 ? (
-                <div className='img-cartCheio'></div>
-              ) : (
-                <div className='img-cartVazio'></div>
-              )}
-            </div>
+        <div className='totalQuantidade-cart'>{getTotalQuantity() || 0} X</div>
+        <div className='totalQuantidade-cart-svg'>
+          {getTotalQuantity() > 0 ? (
+            <div className='img-cartCheio'></div>
+            ) : (
+            <div className='img-cartVazio'></div>
+            )}
         </div>
       </div>
     </Link>
