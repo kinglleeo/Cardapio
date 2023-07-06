3e03313a9ceeba20bb96e1feaf6ef22dc03d950b
kinglleeo <leonardolauerpessoal@outlook.com>
2023-07-06 17:51:43 -0300

    06/07/2023

--- src/components/Paginas/text.jsx
+++ src/components/Paginas/text.jsx
@@ -36,3 +36,24 @@
       }, []);
 
 
+
+
+      <div className='listaPedido-card' key={itemPedido.ID} onClick={()=> selecionarPedido(itemPedido)}>
+                            <div className='pedidoCard-linha'>
+                                <div className='linhaEsquerda'> {itemPedido.TIPOCOMANDA} </div>
+                                <div className='linhaDireita'> {itemPedido.HORA.split(':').slice(0, 2).join(':')} </div>
+                            </div>
+                            <div className='pedidoCard-linha'>
+                                <div className='linhaEsquerda'> Total: {formCurrency.format(itemPedido.VNF_W16)} </div>
+                                <div
+                                    className={
+                                        'statusPedidos ' + 
+                                    (itemPedido.ULTIMO_STATUS_PEDIDO === 'PEDIDO INICIADO' ? 'iniciado' :
+                                    itemPedido.ULTIMO_STATUS_PEDIDO === 'PEDIDO CANCELADO' ? 'cancelado' :
+                                    itemPedido.ULTIMO_STATUS_PEDIDO === 'PEDIDO CONFIRMADO' ? 'confirmado' :
+                                    itemPedido.ULTIMO_STATUS_PEDIDO === 'PENDENTE' ? 'pendente' : '')
+                                    }
+                                    > {capitalizeFirstLetter(itemPedido.ULTIMO_STATUS_PEDIDO.toLowerCase())} </div>
+
+                            </div>
+                        </div>
