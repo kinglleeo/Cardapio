3aa99281449aee3a227775ff100e48c4196422db
kinglleeo <leonardolauerpessoal@outlook.com>
2023-07-06 11:43:55 -0300

    06/07/2023

---
+++ src/components/Administração/Terminal/partes/CorpoTerminal.jsx
@@ -0,0 +1,39 @@
+import { React, useState, useEffect } from 'react'
+
+export default function Terminal(){
+    const [listaPedidos, setListaPedidos] = useState('');
+
+
+    return(
+    <>
+        <div>
+            <div></div>
+            <div>
+                <div></div>
+                <div>
+                    <div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div>
+                        <div>
+                            <div></div>
+                        </div>
+                        <div></div>
+                    </div>
+                </div>
+                <div>
+                    <button></button>
+                    <button></button>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>    
+    </>
+    )
+}
