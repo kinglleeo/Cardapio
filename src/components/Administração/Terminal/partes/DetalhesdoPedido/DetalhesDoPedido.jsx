4e8b2a78d9ea8d8bf1578f2e1a61e532f9c157cd
kinglleeo <leonardolauerpessoal@outlook.com>
2023-07-07 09:23:27 -0300

    07/07/2023

--- src/components/Administração/Terminal/partes/DetalhesdoPedido/DetalhesDoPedido.jsx
+++ src/components/Administração/Terminal/partes/DetalhesdoPedido/DetalhesDoPedido.jsx
@@ -3,7 +3,10 @@ import './detalhesdopedido.css'
 
 
 export default function DetalhesDoPedido({ itemPedido }){
-    
+    const [dadosCompraPedido, setDadosCompraPedido] = useState([]);
+    console.log(itemPedido)
+
+
     const aceitarPedido=()=>{
 
     }
@@ -11,16 +14,38 @@ export default function DetalhesDoPedido({ itemPedido }){
     const cancelarPedido=()=>{
         
     }
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
 
     return(
         <div className='listaDetalhesPedido'>
-            <div>
-
+            <div className='quadroDetalhesPedido'>
+                <div className='quadroPedidoLinha'>
+                    <div className='tipoPedido marginEsquerda textoFonte'> {itemPedido.TIPOCOMANDA} </div> 
+                    <div className='horaPedido marginDireita textoFonte'> {itemPedido.HORA.split(':').slice(0, 2).join(':')} </div>
+                </div>
+                <div className='quadroPedidoLinha'>
+                    <div className='localizacaoPedido'> localização </div>
+                </div>
+                <div className='quadroPedidoLinha'>
+                    <div className='numeroPedido'> Pedido n° {itemPedido.ID} </div>
+                    <div className={
+                      'statusPedidos ' +
+                      (itemPedido.ULTIMO_STATUS_PEDIDO === 'PEDIDO INICIADO' ? 'iniciado'
+                        : itemPedido.ULTIMO_STATUS_PEDIDO === 'PEDIDO CANCELADO' ? 'cancelado'
+                        : itemPedido.ULTIMO_STATUS_PEDIDO === 'PEDIDO CONFIRMADO' ? 'confirmado'
+                        : itemPedido.ULTIMO_STATUS_PEDIDO === 'PENDENTE' ? 'pendente'
+                        : '')
+                    }>
+                        {capitalizeFirstLetter(itemPedido.ULTIMO_STATUS_PEDIDO.toLowerCase())}
+                    </div>
+                </div>
             </div>
-            <div>
-
+            <div className='quadroDetalhesPedido'>
+                <div></div>
             </div>
-            <div>
+            <div className='quadroDetalhesPedido'>
 
             </div>
             <div className='caixaBtnsPedidos'>
