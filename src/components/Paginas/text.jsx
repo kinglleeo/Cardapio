093273a43809d0cdab9e68375e207a8b51b75489
kinglleeo <leonardolauerpessoal@outlook.com>
2023-07-17 10:21:36 -0300

    telainicial

--- src/components/Paginas/text.jsx
+++ src/components/Paginas/text.jsx
@@ -35,3 +35,80 @@
         });
       }, []);
 
+
+      useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const tipoComanda = urlParams.get('tipoComanda');
+        const numeroComanda = urlParams.get('numeroComanda');
+        const cnpj = urlParams.get('cnpj');
+        const login = urlParams.get('login')
+            setCnpj(cnpj)
+        const url = `http://suporte.bedinfoservices.com.br:99/appGarline/retornaApiRestaurante.php?cnpj=${cnpj}`;
+            axios
+                .post(url)
+                .then((response)=>{
+                    setResposta(response);
+                })
+            const Dados = {
+                tipoComanda: tipoComanda,
+                numeroComanda: numeroComanda,
+                cnpj: cnpj
+            }
+            
+            const timeout = setTimeout(() => {
+            if(tipoComanda === "MESA"){
+                localStorage.clear()
+                localStorage.setItem('dados', JSON.stringify(Dados));
+                localStorage.setItem('empresa', JSON.stringify(infoClientes))
+                //navigate('/Main')
+            } 
+            else if(tipoComanda === "CARTAO"){
+                localStorage.clear()
+                localStorage.setItem('dados', JSON.stringify(Dados));
+                localStorage.setItem('empresa', JSON.stringify(infoClientes))
+                //navigate('/Main')
+            }
+            else if (login === "TERMINAL"){{
+                localStorage.clear()
+                localStorage.setItem('dados', JSON.stringify(Dados));
+                localStorage.setItem('login', login);
+                localStorage.setItem('empresa', JSON.stringify(infoClientes))
+                //navigate('/LoginAdm')
+            }}
+            else if (login === "GARCOM"){{
+                localStorage.clear()
+                localStorage.setItem('dados', JSON.stringify(Dados));
+                localStorage.setItem('login', login);
+                localStorage.setItem('empresa', JSON.stringify(infoClientes))
+                //navigate('/LoginGarcom')
+            }}
+            else if (tipoComanda === "DELIVERY"){{
+                localStorage.clear()
+                localStorage.setItem('dados', JSON.stringify(Dados));
+                localStorage.setItem('empresa', JSON.stringify(infoClientes))
+                console.log(infoClientes)
+                //navigate('/Main')
+            }} 
+          }, 3000);
+          return () => {
+            clearTimeout(timeout);
+        };
+    }, [infoClientes, setInfoClientes, setResposta]);
+    
+    useEffect(() => {
+        if (resposta) {
+          const parts = resposta.data.split('|');
+          if (parts.length === 2) {
+            const rotalink = parts[0].trim();
+            const rotaBase = parts[1].trim();
+            const RotaFinal = `${rotalink}:${rotaBase}`;
+                iniciarRota(RotaFinal)
+          }
+        }
+        api
+            .get(`/dadosEmpresa/${cnpj}`)
+            .then((getdata)=>{
+                setInfoClientes(getdata.data);
+            });
+    }, [cnpj, resposta, setInfoClientes]);
+    
